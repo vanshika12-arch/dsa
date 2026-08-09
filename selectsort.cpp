fd611ac2baa1bd07e3af568f07d855e6741a77b6
vanshika singh <itzkhushisingh12@gmail.com>
2026-08-09 21:37:38 +0530

    selection sort

---
+++ selectsort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[100], n, i, j, min, temp;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    // Selection Sort
+    for (i = 0; i < n - 1; i++)
+    {
+        min = i;
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[min])
+            {
+                min = j;
+            }
+        }
+
+        // Swap
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+
+    cout << "Sorted array: ";
+
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
