7c6594ed530009b160d983490493923d1ec2ef23
vanshika singh <itzkhushisingh12@gmail.com>
2026-09-07 13:10:21 +0530

    inorder traversal

---
+++ inorder.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+void inorder(Node* root) {
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+
+    return 0;
+}
